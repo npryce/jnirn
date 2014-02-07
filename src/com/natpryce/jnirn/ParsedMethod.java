ec4f5a50a868549fcc06ec43bdcdacb22c6c9670
time4tea <james@time4tea.net>
2014-02-07 17:06:13 +0000

    supporting Proguard cont...  - used Obfuscatable<Method> in parsed method and make the template use it
    NOTE - the Proguard tests are still not working

--- src/com/natpryce/jnirn/ParsedMethod.java
+++ src/com/natpryce/jnirn/ParsedMethod.java
@@ -4,8 +4,7 @@ import org.objectweb.asm.Type;
 import org.objectweb.asm.commons.Method;
 
 public class ParsedMethod {
-    public final Obfuscatable<String> methodName;
-    public final Method method;
+    public final Obfuscatable<Method> method;
 
     public final boolean overloaded;
     public final boolean isNative;
@@ -14,24 +13,24 @@ public class ParsedMethod {
     public ParsedMethod(Method method, boolean overloaded, boolean isNative, boolean isCalledBack) {
         this.isNative = isNative;
         this.isCalledBack = isCalledBack;
-        this.methodName = Obfuscatable.of(method.getName());
-        this.method = method;
+        this.method = Obfuscatable.of(method);
         this.overloaded = overloaded;
     }
 
     public String descriptor() {
-        return method.getDescriptor();
+        return method.atRuntime.getDescriptor();
     }
 
     public String cmethod() {
-        String v = methodName.inSource.replace("_", "_1");
-        return !overloaded ? v : v + "__" + signatureToC();
+        Method sourceMethod = method.inSource;
+        String v = sourceMethod.getName().replace("_", "_1");
+        return !overloaded ? v : v + "__" + signatureToC(sourceMethod);
     }
 
-    private String signatureToC() {
+    private String signatureToC(Method sourceMethod) {
         StringBuilder b = new StringBuilder();
 
-        for (Type argumentType : method.getArgumentTypes()) {
+        for (Type argumentType : sourceMethod.getArgumentTypes()) {
             b.append(argumentType.getDescriptor().replace("/", "_").replace(";", "_2"));
         }
 
